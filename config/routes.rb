6cad97965bf7753eeab4f909864b0520234bfcdf
github username <github email address>
2017-03-22 11:38:07 -0400

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/guests/:id/new', to: 'appearances#guest_new', as: 'guest_new_appearance'
   get '/episodes/:id/new', to: 'appearances#episode_new', as: 'episode_new_appearance'
   post '/appearances/guests/:guest_id', to: 'appearances#guest_create', as: 'guest_create'
-  post '/appearances/events/:episode_id', to: 'appearances#guest_create', as: 'guest_create'
+  post '/appearances/events/:episode_id', to: 'appearances#episode_create', as: 'episode_create'
 
 
   resources :appearances, only: ["create"]
